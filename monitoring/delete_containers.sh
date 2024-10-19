2e5a19e079d5646a61cec6afcf56289445874cf0
StakeandRelax Validator <107054983+StakeandRelax-Validator@users.noreply.github.com>
2024-10-19 18:23:03 +0200

    Update delete_containers.sh

--- monitoring/delete_containers.sh
+++ monitoring/delete_containers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Delete containers"
-sudo docker-compose down
-sudo docker rm -f $(sudo docker ps -a -q)
-sudo docker volume rm $(sudo docker volume ls -q)
-sudo docker rmi $(sudo docker image ls -q)
+echo "Do you want to delete ALL the containers in the system? y/N"
+read -r DELETE
+if [ "$DELETE" == "Y" ]; then
+    sudo docker-compose down
+    sudo docker rm -f $(sudo docker ps -a -q)
+    sudo docker volume rm $(sudo docker volume ls -q)
+    sudo docker rmi $(sudo docker image ls -q)
+fi
