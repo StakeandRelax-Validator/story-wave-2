018691157a34c46c46170c42a1d1c30a49116e01
StakeandRelax Validator <107054983+StakeandRelax-Validator@users.noreply.github.com>
2024-10-19 18:22:35 +0200

    Update install_start_monitoring.sh

--- monitoring/install_start_monitoring.sh
+++ monitoring/install_start_monitoring.sh
@@ -3,11 +3,7 @@ echo "Installing Docker"
 sudo apt install docker -y
 sudo apt install docker-compose -y
 
-echo "Do you want to delete ALL the container in the system? y/N"
-read -r DELETE
-if [ "$DELETE" == "Y" ]; then
 ./delete_containers.sh
-fi
 
 ./delete_service.sh
 
