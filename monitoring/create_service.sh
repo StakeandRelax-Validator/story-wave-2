20d1770760781137e9130e7312fcaf19992a2ab9
StakeandRelax Validator <107054983+StakeandRelax-Validator@users.noreply.github.com>
2024-10-19 17:47:43 +0200

    Add files via upload

---
+++ monitoring/create_service.sh
@@ -0,0 +1,21 @@
+    echo "Create monitoring.service"
+    sudo systemctl disable montoring.service
+    sudo rm /etc/systemd/system/monitoring.service
+    MONITORING_PATH=$(pwd)
+    sudo tee /etc/systemd/system/monitoring.service > /dev/null <<EOF
+[Unit]
+Description=Docker monitoring containers
+After=docker.service
+Requires=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/bash -c "docker-compose -f $MONITORING_PATH/docker-compose.yaml up --detach"
+ExecStop=/bin/bash -c "docker-compose -f $MONITORING_PATH/docker-compose.yaml stop"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl enable monitoring
